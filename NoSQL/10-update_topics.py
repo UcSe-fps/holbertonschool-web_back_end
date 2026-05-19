5bc76d84f54371327953baa3d9b87bfd00fb11c4
UcSe-fps <rehimliazer184@gmail.com>
2026-05-19 15:26:02 +0000

    try

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+"""comment for comment"""
 
 
 def update_topics(mongo_collection, name, topics):
     """ this will be something"""
     return mongo_collection.update_many({"name": name},
-{$set: {"topics": topics}})
+{"$set": {"topics": topics}})
