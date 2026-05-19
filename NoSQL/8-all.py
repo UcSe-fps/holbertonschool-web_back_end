94eaef43759336a0df7d49264f19f0a71d037261
UcSe-fps <rehimliazer184@gmail.com>
2026-05-19 15:11:51 +0000

    pychart

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""will import pymongo"""
 import pymongo
 
 
 def list_all(mongo_collection):
-    """this will give all the documents in the db"""
-
+    """
+    this will give all the documents in the db
+    """
     return list(mongo_collection.find())
