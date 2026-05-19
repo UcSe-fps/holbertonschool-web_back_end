e41aa42ec9bb3cf83a80c1e64f91512f0c40e3ed
UcSe-fps <rehimliazer184@gmail.com>
2026-05-19 15:31:39 +0000

    real

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""this comment is for something"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ this one too"""
+    return mongo_collection.find({"topic": topic})
