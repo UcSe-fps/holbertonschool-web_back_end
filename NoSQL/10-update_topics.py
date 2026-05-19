59a447605ebc09482eb45aa44311585a6bae9fa6
UcSe-fps <rehimliazer184@gmail.com>
2026-05-19 15:22:54 +0000

    try

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-"""imports pymongo"""
-import pymongo
 
 
 def update_topics(mongo_collection, name, topics):
@@ -8,4 +6,4 @@ def update_topics(mongo_collection, name, topics):
         this will be something
     """
     return mongo_collection.update_many({"name": name},
-            {$set: {"topics": topics}})
+{$set: {"topics": topics}})
