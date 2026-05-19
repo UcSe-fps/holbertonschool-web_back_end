80fbdbb9084a1ba96663d059a1a9c53cde4ad17b
UcSe-fps <rehimliazer184@gmail.com>
2026-05-19 15:33:14 +0000

    real

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-"""this comment is for something"""
+"""
+this comment is for something
+"""
 
 
 def schools_by_topic(mongo_collection, topic):
-    """ this one too"""
+    """ 
+    this one too
+    """
     return mongo_collection.find({"topic": topic})
